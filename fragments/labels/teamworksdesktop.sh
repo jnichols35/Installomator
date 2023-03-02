fcefa19ee68ea61a5a63f3f97b32949c136a8d72
Jake <nicholsjacob35@gmail.com>
2023-03-02 12:34:28 -0500

    add label teamworks desktop

---
+++ fragments/labels/teamworksdesktop.sh
@@ -0,0 +1,7 @@
+teamworkdesktop)
+    name="Teamwork Desktop"
+    type="dmg"
+    downloadURL="https://tw-open.s3.amazonaws.com/projects/electron/releases/teamwork-projects-desktop.dmg"
+    appNewVersion=""
+    expectedTeamID="ES2A6CKPMW"
+    ;;
